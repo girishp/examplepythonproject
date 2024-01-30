2732207773a380dfe2bf11b8ff3686f7e8d8c7f3
Girish Pattabiraman <g_pattabiraman@apple.com>
2024-01-29 16:05:26 -0800

    Starting recipe

---
+++ dal.py
@@ -0,0 +1,6 @@
+print("Recipe for daal");
+print("Steps:");
+print("1. Boil daal");
+print("2. Add salt");
+
+
---
+++ palak_recipe.py
@@ -0,0 +1,7 @@
+print("Recipe for palak paneer");
+print("Steps:");
+print("1. Cut paneer into cubes");
+print("2. Shallow fry paneer cubes");
+print("3. Boil spinach leaves");
+print("4. Add paneer");
+
